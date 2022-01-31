36724db5bdb41152301459e49676e2d29a18fd9a
fmul <fmul@fresnou.org>
2022-01-31 08:38:39 -0800

    inital commit

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -13,14 +13,50 @@ module.exports.viewProfile= async function (req,res){
 }
 
 //render add form
-
+module.exports.renderAddForm=function(req,res){
+    const course= {
+        name:'',
+        department:departments[0],
+        instructor_name: '',
+        description: ''
+    }
+    res.render('course/add',{course,department});
+}
 //add
-
+module.exports.addCourse=async function(req,res){
+    const course=await Course.create({
+        name:req.body.name,
+        department:req.body.department,
+        instructor_name:req.body.instructor_name,
+        description:req.body.description
+    });
+    res.redirect(`/courses/profile/${course.id}`);
+}
 //render edit form
 module.exports.renderEditForm = async function(req, res){
     const course = await Course.findByPk(req.params.id);
     res.render('course/edit', {course,departments});
 }
 //update
-
+module.exports.updateCourse= async function(req,res){
+    const course = await Course.update({
+        name:req.body.name,
+        department:req.body.department,
+        instructor_name:req.body.instructor_name,
+        description:req.body.description
+    },{
+        where:{
+            id:req.params.id
+        }
+        });
+    res.redirect(`/courses/profile/${req.params.id}`);
+}
 //delete
+module.exports.deleteCourse=async function(req,res){
+    await Course.destroy({
+        where:{
+            id:req.params.id
+        }
+    });
+    res.redirect('/courses');
+}
--- routes/index.js
+++ routes/index.js
@@ -11,3 +11,7 @@ router.get('/courses',courseController.viewAll);
 router.get('/courses/profile/:id', courseController.viewProfile);
 router.get('/courses/edit/:id', courseController.renderEditForm);
 module.exports = router;
+router.post('/courses.edit/:id',courseController.updateCourse);
+router.get('/courses/add',courseController.renderAddForm);
+router.post('/courses/add', courseController.addCourse);
+router.get('/courses/delete/:id', courseController.deleteCourse)
