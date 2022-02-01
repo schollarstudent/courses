77a915787681a36a063bcae5780c16156b536997
fmul <fmul@fresnou.org>
2022-02-01 08:55:40 -0800

    inital commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const courseController = require('../controllers/courseController');
-
+const studentController = require('../controllers/studentController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -15,3 +15,12 @@ router.post('/courses.edit/:id',courseController.updateCourse);
 router.get('/courses/add',courseController.renderAddForm);
 router.post('/courses/add', courseController.addCourse);
 router.get('/courses/delete/:id', courseController.deleteCourse)
+
+
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit',studentController.renderEditForm);
+router.post('/student/edit/:id',studentController.updateStudent);
+router.get('/students/add',studentController.renderAddForm);
+router.post('/students/add',studentController.addStudent);
+module.exports=router;
